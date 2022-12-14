05a09e806901dbc425175254fe34d99cd08648d3
Paradigm-Web <120433682+Paradigm-Web@users.noreply.github.com>
2022-12-15 01:03:56 +1300

    Intermediate logic
    Calculations work for 1 operation
    Non BEDMAS calculations work for multiple operations.
    BEDMAS calcs break

--- app.js
+++ app.js
@@ -24,20 +24,21 @@ function multiply(num1, num2) {
 }
 
 // perform an operation
-function operate(num1, num2, operand) {
+function operate(num1, operand, num2) {
     // console.log(`${num1} ${operand.name} ${num2} = `);
-    return operand(num1, num2);
+    return operand(parseFloat(num1), parseFloat(num2));
 }
 
 /**
- * Data Fields
+ * Globals
  */
 
 let output = "0.0";
 let cleared = false;
-let operand = add;
-let number1 = 5;
-let number2 = 7;
+let operand = null;
+let n1 = 5;
+let n2 = 7;
+let inputs = [null, null];
 
 const calculator = document.querySelector('.calculator');
 
@@ -67,24 +68,31 @@ function click(btn, func = () => {null;}) {
 const clear = calculator.querySelector('#clear');
 click(clear, () => {
     updateScreen(`0.0`);
+    operand = null;
     cleared = true;
 });
 
 // equals button handler
 const equals = calculator.querySelector('#equals');
 click(equals, () => {
-    console.log(screen.textContent);
-    console.log(operate(number1, number2, operand));
-    updateScreen(operate(number1, number2, operand));
+    if (operand === null) {
+        updateScreen(inputs[0]);
+    } else updateScreen(operate(inputs[0], operand, inputs[1]));
+    operand = null;
+    inputs = [null, null];
     cleared = true;
 });
 
 // operand button handlers
 const operands = calculator.querySelectorAll('.op');
-operands.forEach((o) => {
-    click(o, () => {
-        operand = window[o.id];
-        updateScreen(output.concat(o.value));
+operands.forEach((op) => {
+    click(op, () => {
+        if (operand !== null) {
+            inputs[0] = operate(inputs[0], operand, inputs[1]).toString();
+            inputs[1] = null;
+        }
+        operand = window[op.id];
+        updateScreen(output.concat(op.value));
     });
 });
 
@@ -93,10 +101,28 @@ const numbers = calculator.querySelectorAll('.num');
 numbers.forEach((n) => {
     click(n, () => {
         if (cleared) output = "";
+        if (operand === null) {
+            if (inputs[0] !== null) {
+                inputs[0] = inputs[0].concat(n.value);
+            } else inputs[0] = n.value;
+            
+        }  else {
+            if (inputs[1] !== null) {
+                inputs[1] = inputs[1].concat(n.value);
+            } else inputs[1] = n.value;    
+        }
+        console.log(inputs);
         cleared = false;
         updateScreen(output.concat(n.value));
-        num1 = n.value;
+        
     });
 });
 
+/**
+ * Operation Logic
+ */
+
+// else if (inputs[0] != null && inputs[1] != null) {
+//     inputs[0] = operate(inputs[0], operand, inputs[1]);
+// }
 
